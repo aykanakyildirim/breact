687e9bddf41d7009bff2f28b98e05064f207444f
aykanakyildirim <akyildirim.aykan@gmail.com>
2024-06-12 18:29:11 +0200

    first commit

---
+++ my-react-app-110624/src/Header.jsx
@@ -0,0 +1,22 @@
+
+function Header(){
+
+    return(
+        <header>
+            <h1>
+                My Website
+            </h1>
+            <nav>
+                <u1>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </u1>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
