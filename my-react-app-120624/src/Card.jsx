449d566366f5a3943fb889aa9bddb0c272493506
aykanakyildirim <akyildirim.aykan@gmail.com>
2024-06-12 19:43:51 +0200

    Card components added. Css styling restarted.

---
+++ my-react-app-120624/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://media.printables.com/media/prints/451882/images/3720069_73330541-f134-4f9f-ab7d-d9e57719e5fd/shadow-sonic.jpg" alt="profile pic"></img>
+            <h2 className="card-title">Aykan</h2>
+            <p className="card-text">I am a Computer Engineer and a casual history nerd</p>
+        </div>
+    );
+} 
+
+export default Card
