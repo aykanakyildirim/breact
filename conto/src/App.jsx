c9badefbf357c9801a1fb593d7ea037c2aa77605
aykanakyildirim <akyildirim.aykan@gmail.com>
2024-06-20 02:41:29 +0200

    count dev started with useEffect functionalities

---
+++ conto/src/App.jsx
@@ -0,0 +1,14 @@
+import MyComponent from "./MyComponent"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <MyComponent/>
+    </>
+  )
+}
+
+export default App
