83dcb999e00ac601258c814d82da68d2ae637e0e
aykanakyildirim <akyildirim.aykan@gmail.com>
2024-06-20 02:49:33 +0200

    adjusted dependencies for useEffect

--- conto/src/App.jsx
+++ conto/src/App.jsx
@@ -2,13 +2,12 @@ import MyComponent from "./MyComponent"
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <MyComponent/>
     </>
-  )
+  );
 }
 
 export default App
--- conto/src/MyComponent.jsx
+++ conto/src/MyComponent.jsx
@@ -2,18 +2,30 @@ import React, {useState, useEffect} from "react";
 
 function MyComponent(){
     const [count, setCount]=useState(0);
+    const [color, setColor]=useState("green");
     
-    useEffect(()=>{
-        document.title= 'Count: ${count}';
-    }, [count]);
+    useEffect(() => {
+        document.title= 'Count: ${count} ${color}';
+    }, [count, color]);
 
     function addCount(){
         setCount(c=>c+1);
     }
 
+    function subtractCount(){
+        setCount(c=>c-1);
+    }
+
+    function changeColor(){
+        setColor(c=>c === "green" ? "red":"green");
+    }
+
     return(<>
-        <p>Count:{count}</p>
+        <p style={{color:color}}>Count:{count}</p>
         <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button><br/>
+        <button onClick={changeColor}>Change Color</button>
+
     </>);
 }
 
