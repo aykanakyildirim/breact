f965581fb467d5ef18a84780cabbe55526cf52b2
aykanakyildirim <akyildirim.aykan@gmail.com>
2024-06-20 03:09:22 +0200

    event listeners and cleanup added

--- conto/src/MyComponent.jsx
+++ conto/src/MyComponent.jsx
@@ -3,9 +3,31 @@ import React, {useState, useEffect} from "react";
 function MyComponent(){
     const [count, setCount]=useState(0);
     const [color, setColor]=useState("green");
-    
+
+    const [width, setWidth]=useState(window.innerWidth);
+    const [height, setHeight]=useState(window.innerHeight);
+
+    //event Listener without useEffect 
+    // window.addEventListener("resize", handleResize);
+    // invokes too many eventListeners: every time there is a change
+
+    //adding an event listener for only when the component mounts
+    useEffect(()=>{
+        window.addEventListener("resize", handleResize);
+
+        return()=>{
+            window.removeEventListener("resize", handleResize);
+            console.log("Event Listener Removed!");
+        }
+    }, []);
+
     useEffect(() => {
-        document.title= 'Count: ${count} ${color}';
+        document.title= `Count: ${count} ${color}`;
+
+        return ()=>{
+            //SOME CLEANUP CODE BEFORE THE NEXT RE-RENDER OR MOUNT
+
+        }
     }, [count, color]);
 
     function addCount(){
@@ -20,12 +42,19 @@ function MyComponent(){
         setColor(c=>c === "green" ? "red":"green");
     }
 
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
     return(<>
         <p style={{color:color}}>Count:{count}</p>
         <button onClick={addCount}>Add</button>
         <button onClick={subtractCount}>Subtract</button><br/>
         <button onClick={changeColor}>Change Color</button>
 
+        <p>Window Width: {width}px</p>
+        <p>Window Height: {height}px</p>
     </>);
 }
 
