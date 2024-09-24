3bff893f90d186afbc10c08fa656e81f3ee910d8
aykanakyildirim <akyildirim.aykan@gmail.com>
2024-09-25 00:17:23 +0200

    modified:   fastest/main.py and the test_server.py

--- fastest/test_server.py
+++ fastest/test_server.py
@@ -1,2 +1,3 @@
 import requests
-print(requests.get("http://localhost:8000/items/0", params={"name": "Hammer"}).json())
+print(requests.get("http://localhost:8000/items/0").json())
+print(requests.get("http://localhost:8000/items?name=Nails").json())
